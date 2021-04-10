91cd0d20b340491cd50613e80fb86392a3a2b49b
ignaciollb <ignacio.llanos1601@alumnos.ubiobio.cl>
2021-04-09 22:33:41 -0400

    parte 1 intro a angular

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,8 +9,10 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
-  
 
+  onNotify(){
+    window.alert('You will be notified when the product goes on sale')
+  }
   share() {
     window.alert('The product has been shared!');
   }
